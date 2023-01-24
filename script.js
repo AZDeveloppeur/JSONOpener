326af3cb37c5066ee7e27de0beb419ba5f89c20a
AZDeveloppeur <developpeur.az@gmail.com>
2023-01-24 19:58:15 +0100

    Remove html and add package.json

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("files").onchange = (e) => { 
-    var file = e.target.files[0]; 
-    var reader = new FileReader();
-    reader.readAsText(file,'UTF-8');
-    reader.onload = (readerEvent) => {
-        var content = readerEvent.target.result;
-        console.log(content);
-    }
-}
